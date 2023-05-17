ee108580f740c1babe6326cfa33be1952f098e87
lsst25 <lsst25@gmail.com>
2023-05-17 23:16:39 +0300

    refactor: sort imports

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,11 +1,11 @@
+import React from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import {GameQuery} from "../App";
 import {SimpleGrid, Spinner, Text} from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
-import {GameQuery} from "../App";
-import React from "react";
-import InfiniteScroll from "react-infinite-scroll-component";
 
 interface Props {
     gameQuery: GameQuery
