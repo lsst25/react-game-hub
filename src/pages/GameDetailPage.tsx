e9da3ec72ddcfefe1a605491b5c638d9524a5de6
lsst25 <lsst25@gmail.com>
2023-05-19 23:21:32 +0300

    feat: set up routing

---
+++ src/pages/GameDetailPage.tsx
@@ -0,0 +1,10 @@
+
+const GameDetailPage = () => {
+    return (
+        <div>
+            GameDetailPage
+        </div>
+    )
+};
+
+export default GameDetailPage;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,38 @@
+import {Flex, Grid, GridItem, Show} from "@chakra-ui/react";
+import GenreList from "../components/GenreList";
+import GameHeading from "../components/GameHeading";
+import PlatformSelector from "../components/PlatformSelector";
+import SortSelector from "../components/SortSelector";
+import GameGrid from "../components/GameGrid";
+
+const layouts = {
+    base: `"main"`,
+    lg: `"aside main"`,
+}
+
+const columns = {
+    base: "1fr",
+    lg: "200px 1fr",
+}
+
+const HomePage = () => {
+    return (
+        <Grid templateAreas={layouts} templateColumns={columns}>
+            <Show above="lg">
+                <GridItem gridArea="aside" paddingX={5}>
+                    <GenreList/>
+                </GridItem>
+            </Show>
+            <GridItem gridArea="main" padding={3}>
+                <GameHeading/>
+                <Flex gap={2} marginBottom={3}>
+                    <PlatformSelector/>
+                    <SortSelector/>
+                </Flex>
+                <GameGrid/>
+            </GridItem>
+        </Grid>
+    );
+}
+
+export default HomePage;
