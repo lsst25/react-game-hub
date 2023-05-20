6139065ca6c7fcbe461990841978e4b84a601fec
lsst25 <lsst25@gmail.com>
2023-05-20 12:50:08 +0300

    feat: implement expandable description

---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,32 @@
+import {useState} from "react";
+import {Button, Text} from "@chakra-ui/react";
+
+interface Props {
+    children: string;
+    maxChars?: number;
+}
+
+const ExpandableText = ({children, maxChars = 300}: Props) => {
+    const [expanded, setExpanded] = useState(false);
+
+    if (!children) return null;
+
+    if (children.length <= maxChars) {
+        return (
+            <Text>{children}</Text>
+        );
+    }
+
+    const text = expanded ? children : `${children.slice(0, maxChars)}...`;
+
+    return (
+        <>
+            <Text>{text}</Text>
+            <Button fontWeight="bold" colorScheme="yellow" size="xs" onClick={() => setExpanded(!expanded)}>
+                {expanded ? "Show less" : "Show more"}
+            </Button>
+        </>
+    );
+};
+
+export default ExpandableText;
