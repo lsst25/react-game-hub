5c16c2098b11842e26b0d82d4e5d0c8a98ea85da
lsst25 <lsst25@gmail.com>
2023-05-20 15:12:08 +0300

    fix: nav bar logo and search

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,15 +2,20 @@ import {Input, InputGroup, InputLeftElement} from "@chakra-ui/react";
 import {BsSearch} from "react-icons/all";
 import {useRef} from "react";
 import useGameQueryStore from "../store";
+import {useNavigate} from "react-router-dom";
 
 const SearchInput = () => {
+    const navigate = useNavigate();
     const setSearchText = useGameQueryStore(s => s.setSearchText);
     const inputRef = useRef<HTMLInputElement>(null);
 
     return (
         <form onSubmit={(e) => {
             e.preventDefault();
-            if (inputRef.current) setSearchText(inputRef.current.value);
+            if (inputRef.current) {
+                setSearchText(inputRef.current.value);
+                navigate('/');
+            }
         }}>
             <InputGroup>
                 <InputLeftElement children={<BsSearch/>}/>
