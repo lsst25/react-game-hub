4c1076dbdec225fd2e157972e355b57ef88993fe
lsst25 <lsst25@gmail.com>
2023-05-20 13:18:54 +0300

    feat: build game attributes

---
+++ src/components/GameAttributes.tsx
@@ -0,0 +1,38 @@
+import DefinitionItem from "./DefinitionItem";
+import {SimpleGrid, Text} from "@chakra-ui/react";
+import CriticScore from "./CriticScore";
+import {Game} from "../interfaces/game";
+
+interface Props {
+    game: Game;
+}
+
+const GameAttributes = ({ game }: Props) => {
+    return (
+        <SimpleGrid columns={2} as="dl">
+            <DefinitionItem term="Platforms">
+                {game.parent_platforms.map(({platform}) => (
+                    <Text key={platform.id}>{platform.name}</Text>
+                ))}
+            </DefinitionItem>
+
+            <DefinitionItem term="Meta-score">
+                <CriticScore score={game.metacritic}/>
+            </DefinitionItem>
+
+            <DefinitionItem term="Genres">
+                {game.genres.map(({name, id}) => (
+                    <Text key={id}>{name}</Text>
+                ))}
+            </DefinitionItem>
+
+            <DefinitionItem term="Publishers">
+                {game.publishers.map(({name, id}) => (
+                    <Text key={id}>{name}</Text>
+                ))}
+            </DefinitionItem>
+        </SimpleGrid>
+    );
+};
+
+export default GameAttributes;
--- src/interfaces/game.ts
+++ src/interfaces/game.ts
@@ -1,8 +1,12 @@
 import {Platform} from "./platform";
+import {Genre} from "./genre";
+import {Publisher} from "./publisher";
 
 export interface Game {
     id: number;
     name: string;
+    genres: Genre[];
+    publishers: Publisher[];
     background_image: string;
     parent_platforms: { platform: Platform }[];
     metacritic: number;
